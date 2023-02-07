0f925cead06c507e25200eb77c0316eddfbf6484
Pavel <pavel.nik03@mail.ru>
2023-02-07 14:19:21 +0700

    added babel, extract plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -4,9 +4,41 @@ import {BuildOptions} from "./types/config";
 
 export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
 
+    const fileLoader = {
+        test: /\.(png|jpe?g|gif|woff|woff2)$/i,
+        use: [
+            {
+                loader: 'file-loader'
+            }
+        ]
+    }
+
     const svgLoader = {
         test: /\.svg$/,
         use: ['@svgr/webpack']
+    }
+
+    const babelLoader = {
+        test: /\.(js|jsx|ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+            loader: "babel-loader",
+            options: {
+                presets: ['@babel/preset-env'],
+                "plugins": [
+                    [
+                        "i18next-extract",
+                        {
+                            "locales": [
+                                "ru",
+                                "en"
+                            ],
+                            "KeyAsDefaultValue": true
+                        }
+                    ]
+                ]
+            }
+        }
     }
 
     const cssLoader = {
@@ -34,19 +66,11 @@ export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
         exclude: /node_modules/,
     }
 
-    const fileLoader = {
-        test: /\.(png|jpe?g|gif|woff|woff2)$/i,
-        use: [
-            {
-                loader: 'file-loader'
-            }
-        ]
-    }
-
     return [
+        fileLoader,
         svgLoader,
+        babelLoader,
         typescriptLoader,
         cssLoader,
-        fileLoader
     ]
 }
