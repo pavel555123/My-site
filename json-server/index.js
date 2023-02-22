2272a2baf6989d8fa25bf492ec13ac5d6770df5b
Pavel <pavel.nik03@mail.ru>
2023-02-22 17:39:10 +0700

    fixed errors

--- json-server/index.js
+++ json-server/index.js
@@ -6,6 +6,10 @@ const server = jsonServer.create()
 
 const router = jsonServer.router(path.resolve(__dirname, 'db.json'))
 
+server.use(jsonServer.defaults({}))
+server.use(jsonServer.bodyParser)
+
+// Нужно для небольшой задержки, чтобы запрос проходил не мгновенно, имитация реального апи
 server.use(async (req, res, next) => {
     // eslint-disable-next-line promise/param-names
     await new Promise((res) => {
@@ -14,32 +18,41 @@ server.use(async (req, res, next) => {
     next()
 })
 
+// Эндпоинт для логина
+server.post('/login', (req, res) => {
+    try {
+        const { username, password } = req.body
+        const db = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'db.json'), 'UTF-8'))
+        const { users = [] } = db
+
+        const userFromBd = users.find(
+            (user) => user.username === username && user.password === password
+        )
+
+        if (userFromBd) {
+            return res.json(userFromBd)
+        }
+
+        return res.status(403).json({ message: 'User not found' })
+    } catch (e) {
+        console.log(e)
+        return res.status(500).json({ message: e.message })
+    }
+})
+
+// проверяем, авторизован ли пользователь
+// eslint-disable-next-line
 server.use((req, res, next) => {
     if (!req.headers.authorization) {
         return res.status(403).json({ message: 'AUTH ERROR' })
     }
+
     next()
 })
 
-server.use(jsonServer.defaults())
 server.use(router)
 
-server.post('/login', (req, res) => {
-    const { username, password } = req.body
-    const db = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'db.json'), 'UTF-8'))
-    const { users } = db
-
-    const userFromBd = users.find(
-        (user) => user.username === username && user.password === password
-    )
-
-    if (userFromBd) {
-        return res.json(userFromBd)
-    }
-
-    return res.status(403).json({ message: 'AUTH_ERROR' })
-})
-
+// запуск сервера
 server.listen(8000, () => {
-    console.log('Server is running on 8000 port')
+    console.log('server is running on 8000 port')
 })
