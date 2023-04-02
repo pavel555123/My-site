6d8ba0ed7f81e98b1ab3cbd88cf05218375828e3
Pavel <pavel.nik03@mail.ru>
2023-04-02 13:36:52 +0700

    fixed api config

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -2,8 +2,13 @@ import axios from 'axios'
 import { USER_LOCALSTORAGE_KEY } from 'shared/const/localStorage'
 
 export const $api = axios.create({
-    baseURL: API,
-    headers: {
-        authorization: localStorage.getItem(USER_LOCALSTORAGE_KEY)
+    baseURL: API
+})
+
+$api.interceptors.request.use((config) => {
+    if (config.headers) {
+        config.headers.Authorization = localStorage.getItem(USER_LOCALSTORAGE_KEY) || ''
     }
+
+    return config
 })
