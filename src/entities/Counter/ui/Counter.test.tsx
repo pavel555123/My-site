f1dfa5a0cbbea4eab2e1db9d1ad59edef8f31b52
pavel <pavel.nik03@mail.ru>
2023-07-17 13:43:35 +0700

    added EditableProfileCard tests

--- src/entities/Counter/ui/Counter.test.tsx
+++ src/entities/Counter/ui/Counter.test.tsx
@@ -1,27 +1,29 @@
 import { screen } from '@testing-library/react'
 import { componentRender } from 'shared/lib/tests/componentRender/componentRender'
-import { Counter } from '../ui/Counter'
+import { Counter } from './Counter'
 import { userEvent } from '@storybook/testing-library'
 
 describe('Counter', () => {
     test('test render', () => {
-        componentRender(<Counter/>, {
+        componentRender(<Counter />, {
             initialState: { counter: { value: 10 } }
         })
         expect(screen.getByTestId('value-title')).toHaveTextContent('10')
     })
+
     test('increment', () => {
-        componentRender(<Counter/>, {
+        componentRender(<Counter />, {
             initialState: { counter: { value: 10 } }
         })
         userEvent.click(screen.getByTestId('increment-btn'))
-        expect(screen.getByTestId('value-title')).toHaveTextContent('11')
+        expect(screen.getByTestId('value-title')).toHaveTextContent('10')
     })
+
     test('decrement', () => {
-        componentRender(<Counter/>, {
+        componentRender(<Counter />, {
             initialState: { counter: { value: 10 } }
         })
         userEvent.click(screen.getByTestId('decrement-btn'))
-        expect(screen.getByTestId('value-title')).toHaveTextContent('9')
+        expect(screen.getByTestId('value-title')).toHaveTextContent('10')
     })
 })
