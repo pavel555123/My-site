a6d961efc10ef658f960c51d532052cd1bdc8826
pavel <pavel.nik03@mail.ru>
2023-08-01 18:23:32 +0700

    upgraded webpack config, added babel cache

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -9,24 +9,17 @@ import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin'
 import { type BuildOptions } from './types/config'
 
 export function buildPlugins ({ paths, isDev, apiUrl, project }: BuildOptions): webpack.WebpackPluginInstance[] {
+    const isProd = !isDev
+
     const plugins = [
         new HTMLWebpackPlugin({
             template: paths.html
         }),
         new webpack.ProgressPlugin(),
-        new MiniCssExtractPlugin({
-            filename: 'css/[name].[contenthash:8].css',
-            chunkFilename: 'css/[name].[contenthash:8].css'
-        }),
         new webpack.DefinePlugin({
             IS_DEV: JSON.stringify(isDev),
             API: JSON.stringify(apiUrl),
             PROJECT: JSON.stringify(project)
-        }),
-        new CopyPlugin({
-            patterns: [
-                { from: paths.locales, to: paths.buildLocales }
-            ]
         }),
         new CircularDependencyPlugin({
             exclude: /node_modules/,
@@ -48,6 +41,22 @@ export function buildPlugins ({ paths, isDev, apiUrl, project }: BuildOptions): 
         plugins.push(new BundleAnalyzerPlugin({
             openAnalyzer: false
         }))
+    }
+
+    if (isProd) {
+        plugins.push(
+            new MiniCssExtractPlugin({
+                filename: 'css/[name].[contenthash:8].css',
+                chunkFilename: 'css/[name].[contenthash:8].css'
+            })
+        )
+        plugins.push(
+            new CopyPlugin({
+                patterns: [
+                    { from: paths.locales, to: paths.buildLocales }
+                ]
+            })
+        )
     }
 
     return plugins
