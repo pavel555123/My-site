ea5bce4f906706532dd5f1e4aac1510750887218
pavel <pavel.nik03@mail.ru>
2023-08-04 12:09:10 +0700

    updated maim.yml for GitHub actions

--- src/entities/Notification/ui/NotificationItem/NotificationItem.stories.tsx
+++ src/entities/Notification/ui/NotificationItem/NotificationItem.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { type ComponentMeta, type ComponentStory } from '@storybook/react'
-
+import withMock from 'storybook-addon-mock'
 import { NotificationItem } from './NotificationItem'
 
 export default {
@@ -8,10 +8,27 @@ export default {
     component: NotificationItem,
     argTypes: {
         backgroundColor: { control: 'color' }
-    }
+    },
+    decorators: [withMock]
 } as ComponentMeta<typeof NotificationItem>
 
 const Template: ComponentStory<typeof NotificationItem> = (args) => <NotificationItem {...args} />
 
 export const Normal = Template.bind({})
 Normal.args = {}
+Normal.parameters = {
+    mockData: [
+        {
+            url: `${API}/notifications`,
+            method: 'GET',
+            status: 200,
+            response: [
+                {
+                    id: '1',
+                    title: '123',
+                    description: 'abc'
+                }
+            ]
+        }
+    ]
+}
