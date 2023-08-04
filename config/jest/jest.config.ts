a388f2ed443451c4c3b9fd56223cd473002fb97d
pavel <pavel.nik03@mail.ru>
2023-08-04 12:33:02 +0700

    added report to CI

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -48,7 +48,7 @@ export default {
         ['jest-html-reporters', {
             publicPath: '<rootDir>/reports/unit',
             filename: 'report.html',
-            openReport: true,
+            // openReport: true,
             inlineSource: true
         }]
     ]
