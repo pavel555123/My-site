deee9a854aefd5ece9d9a667af31036da1fdf207
pavel <pavel.nik03@mail.ru>
2023-08-20 12:56:17 +0700

    added test for isolated components with cypress

--- webpack.config.ts
+++ webpack.config.ts
@@ -12,10 +12,11 @@ export default (env: BuildEnv) => {
         buildLocales: path.resolve(__dirname, 'dist', 'locales')
     }
 
-    const mode = env.mode || 'development'
-    const PORT = env.port || 3000
+    const mode = env?.mode || 'development'
+    const PORT = env?.port || 3000
+    const apiUrl = env?.apiUrl || 'http://localhost:8000'
+
     const isDev = mode === 'development'
-    const apiUrl = env.apiUrl || 'http://localhost:8000'
 
     console.log(isDev, mode, PORT)
 
